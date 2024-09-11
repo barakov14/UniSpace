a487405d8ba59f7527de78895542844d069b0efd
barakov14 <playmagma4@gmail.com>
2024-09-11 14:01:57 +0500

    icons list added

---
+++ libs/core/ui/src/lib/icons-list.ts
@@ -0,0 +1,10 @@
+import {
+  MailOutline,
+  LockOutline,
+  UserOutline
+} from '@ant-design/icons-angular/icons'
+import { IconDefinition } from '@ant-design/icons-angular';
+
+export const icons: IconDefinition[] = [
+  MailOutline, LockOutline, UserOutline
+];
